40e5a160eecb00aaf3afacb94cc522254f30994b
Monyck <wesleyricardo910@gmail.com>
2023-01-24 11:02:16 -0300

    Remove

--- testeProjeto/cliente/src/components/ListaDecrescente.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-class ListaDecrescente extends React.Component{
-    constructor(props){
-        super(props)
-        
-    }
-}
--- testeProjeto/cliente/src/components/OlaMundoFuncao.js
+++
@@ -1,5 +0,0 @@
-function OlaMundoFuncao(props) {
-    return <h1>Olá, {props.nome}</h1>;
-}
-
-export default OlaMundoFuncao;
